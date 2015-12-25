1ab36bc17e46cf3b94c12ddb1e5d12fdec7fff1b
janhavim <janhavim@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-12-25 07:46:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/genenetbp@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -37,33 +37,34 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 The geneNetBP package provides functions to fit Conditional Gaussian Bayesian network to genotype-phenotype or Quantitative Trait Loci (QTL) data, absorb evidence in the genotype-phenotype network and quantify and visualize the changes in network beliefs. 
 
-The package makes extensive use of RHugin package that provides an R interface for the Hugin Decision Engine (HDE) which is a commercial software developed by <a href="http://www.hugin.com">HUGIN EXPERT A/S</a> for building and making inference from Bayesian belief networks.  The RHugin package provides an interface to communicate with the HDE from within R. Please note that RHugin is currently not hosted on CRAN. The package RHugin is available on R-Forge and can be obtained <a href="http://r-forge.r-project.org/projects/rhugin/">here</a>. 
+The package makes extensive use of RHugin package that provides an R interface for the Hugin Decision Engine (HDE) which is a commercial software developed by <a href="http://www.hugin.com">HUGIN EXPERT A/S</a> for building and making inference from Bayesian belief networks.  The RHugin package provides an interface to communicate with the HDE from within R. Please note that RHugin is currently not hosted on CRAN. The package RHugin is available on R-Forge and can be obtained <a http://rhugin.r-forge.r-project.org/">here</a>. 
 
 <h2>Installation</h2>
 
-The geneNetBP package requires HDE and the R package RHugin to be installed to function. Here 
+The geneNetBP package requires HDE and the R package RHugin to be installed to function. 
 
-<p> Installation Steps </p>
+<h3> Installation Steps </h3>
 1. Install Hugin Decision Engine
 2. Install the corresponding version of RHugin package
 3. Install other package dependencies (Rgraphviz, scales)
 4. Install the geneNetBP package
 
-<p> The geneNetBP package. </p>
+<h3> The geneNetBP package. </h3>
 
 The geneNetBP package is currently available on R-Forge and not on CRAN. It can be downloaded <a href="https://r-forge.r-project.org/R/?group_id=2155">here</a>. Alternatively you can install it by R install command
 
-<pre> install.packages(&quot;geneNetBP&quot;, repos=(&quot;http://R-Forge.R-project.org&quot;)</pre>
+<pre> install.packages(&quot;geneNetBP&quot;, repos=(&quot;http://R-Forge.R-project.org&quot;))</pre>
 
-<p> HUGIN Decision Engine.</p>
+<h3> HUGIN Decision Engine.</h3>
 
-The geneNetBP package is compatible with the free demo version of Hugin Researcher/Developer, Hugin Lite Demo that can be obtained from http://www.hugin.com/productsservices/demo/hugin-lite. The free demo version is limited to handle only 50 states and 500 cases.    
+The geneNetBP package is compatible with the free demo version of Hugin Researcher/Developer, Hugin Lite Demo that can be obtained from <a> http://www.hugin.com/productsservices/demo/hugin-lite </a>. The free demo version is limited to handle only 50 states and 500 cases.    
 If you do not have Hugin installed in the default location you will need to set the HUGINHOME environment variable before using the RHugin package. Also, you will need to modify the HUGINHOME variable. Please see the instructions on the project homepage for <a href="http://rhugin.r-forge.r-project.org/">RHugin</a>
 
-<h4>Package Dependencies</h4>
+<h3>Package Dependencies</h3>
 
-<p> RHugin
-geneNetBP package depends on RHugin package which is not on CRAN, installing geneNetBP will not automatically install RHugin. Installation instructions for RHugin can be found on its project homepage http://rhugin.r-forge.r-project.org/
+<p> RHugin </p>
+
+geneNetBP package depends on RHugin package which is not on CRAN, installing geneNetBP will not automatically install RHugin. Installation instructions for RHugin can be found on its project homepage <a> http://rhugin.r-forge.r-project.org/ </a>
 
 It is important to install the matching versions and the architecture (32/64 bit) of Hugin Lite, RHugin and R as listed on RHugin project homepage.
 
