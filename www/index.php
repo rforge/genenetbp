b68f21a79fc98c09190cf8d5835b8089d716596a
janhavim <janhavim@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-01 17:40:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/genenetbp@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -40,7 +40,7 @@ The geneNetBP package provides functions to fit Conditional Gaussian Bayesian ne
 <hr />
 <h2> Download </h2>
 
-The geneNetBP package version 1.0.0 is now available and can be downloaded from <a href="https://cran.r-project.org/web/packages/geneNetBP/">CRAN</a>. 
+The <code>geneNetBP</code> package version 1.0.0 is now available and can be downloaded from <a href="https://cran.r-project.org/web/packages/geneNetBP/">CRAN</a>. 
 <hr/>
 
 <hr />
@@ -51,36 +51,38 @@ The geneNetBP package makes extensive use of RHugin package that provides an R i
 
 The geneNetBP package requires HDE and the R package RHugin to be installed. 
 
-<dl> Installation Steps 
-	<dt> 1. Install Hugin Decision Engine: </dt>
-	<dd> The geneNetBP package is compatible with the free 	demo version of Hugin Researcher/Developer, Hugin Lite 	Demo that can be obtained from <a 	href="http://www.hugin.com/productsservices/demo/hugin-	lite"> HUGIN</a>. The free demo version is limited to 	handle only 50 states and 500 cases.    
+<dl><h3> Installation Steps</h3> 
+	<dt><b> 1. Install Hugin Decision Engine:</b> </dt>
+	<dd> The <code>geneNetBP</code> package is compatible with 	the free 	demo version of Hugin Researcher/Developer, 	Hugin Lite 	Demo that can be obtained from <a 	href="http://www.hugin.com/productsservices/demo/hugin-	lite"> HUGIN</a>. The free demo version is limited to 	handle only 50 states and 500 cases.    
 	If you do not have Hugin installed in the default location 	you will need to set the HUGINHOME environment variable 	before using the RHugin package. Also, you will need to 	modify the HUGINHOME variable. Please see the instructions 	on the project homepage for <a href="http://rhugin.r-	forge.r-project.org/">RHugin</a>.
 	</dd>
 
-	<dt> 2. Install RHugin package <:/dt>
-	<dd> Installation instructions for RHugin can be found 	on its project homepage <a href="http://rhugin.r-	forge.r-project.org/"> RHugin</a>.It is important to 	install the matching versions and the architecture (32/64 	bit) of Hugin Lite, RHugin and R as listed on RHugin 	project homepage.Please note that RHugin is required for 	proper functioning of geneNetBP. The package RHugin will 	not automatically load upon loading geneNetBP package. 	Please use library(RHugin) or require(RHugin) to load it 	before using geneNetBP.
+	<dt><b> 2. Install RHugin package:</b> </dt>
+	<dd> Installation instructions for RHugin can be found 	on its project homepage <a href="http://rhugin.r-	forge.r-project.org/"> RHugin</a>.It is important to 	install the matching versions and the architecture (32/64 	bit) of Hugin Lite, <code>RHugin</code> and R as listed on 	RHugin 	project homepage.Please note that RHugin is 	required for 	proper functioning of geneNetBP. The 	package RHugin will 	not automatically load upon loading 	geneNetBP package. 	Please use library(RHugin) or require	(RHugin) to load it 	before using geneNetBP.
 	</dd>
 
-	<dt> 3. Install other package dependencies:</dt> 
-	<dd> Both geneNetBP and RHugin depend on the Bioconductor 	packages <code>graph</code> and <code>Rgraphviz</code>. 	Run the commands
+	<dt><b> 3. Install other package dependencies:</b></dt> 
+	<dd> Both <code>geneNetBP</code> and <code>RHugin</code> 	depend on the Bioconductor 	packages <code>graph	</code> and <code>Rgraphviz</code>. 	Run the following 	commands to install them.
 	<pre>
   	source(&quot;http://bioconductor.org/biocLite.R&quot;)
   	biocLite(c(&quot;graph&quot;, &quot;Rgraphviz&quot;))
 	</pre>
-
-	<p>to install them.</p>
-
-	<p> scales: </p>The package <code>scales</code> should 	automatically get installed with the package <geneNetBP>. 	If it does not, then you can manually install it. It is 	available on CRAN. Use R install command
-
+	<p> scales: </p>The package <code>scales</code> should 	automatically get installed with the package <code>	geneNetBP</code>. If it does not, then you can manually 	install it. It is available on CRAN. Use R install 	command
 	<pre> install.packages(&quot;scales&quot;)</pre>
 	</dd>
 	
-	<dt> 4. Install the geneNetBP package: </dt>
-	<dd> The geneNetBP package version 1.0.0 is now available 	and can be downloaded from <a href="https://cran.r-	project.org/web/packages/geneNetBP/">CRAN</a>. 
+	<dt><b> 4. Install the geneNetBP package:</b> </dt>
+	<dd> The geneNetBP package version 1.0.0 can be downloaded 	from <a href="https://cran.r-	project.org/web/packages/geneNetBP/">CRAN</a>. 
 	</dd>
 
 </dl>
 <hr/>
+
+<h2>Documentation</h2>
+
+<h2>Examples</h2>
+
+<h2>References</h2>
 
 </body>
 </html>
