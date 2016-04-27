db237549be848caaf19b1b4d150324f691ebf4f2
janhavim <janhavim@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-27 01:25:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/genenetbp@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/fit.dbn.R
@@ -0,0 +1,201 @@
+###################################################################################
+# \name{fit-methods}
+# \alias{fit.gnbp}
+# \alias{fit.dbn}
+# %- Also NEED an '\alias' for EACH other topic documented here.
+# \title{Fit a Conditional Gaussian Bayesian Network or Discrete Bayesian Network to QTL data}
+# 
+# \description{Learn the structure of a genotype-phenotype network from quantitative trait loci (QTL) data and the conditional probability table for each node in the network. 
+# }
+# \usage{
+#   ## Fit a conditional gaussian or a discrete bayesian network using RHugin.
+#   fit.gnbp(geno,pheno,constraints,learn="TRUE",graph,type ="cg",
+#            alpha=0.001,tol=1e-04,maxit=0)
+#   ## Fit a discrete bayesian network using bnlearn.
+#   fit.dbn(geno,pheno,graph,learn="TRUE",method="hc",whitelist,blacklist)
+# }
+# %- maybe also 'usage' for other objects documented here.
+# \arguments{
+#   \item{geno}{a data frame of column vectors of class factor (or one that can be coerced to that class) and non-empty column names.
+#               
+#   }
+#   \item{pheno}{a data frame of column vectors of class numeric for \code{fit.gnpb} if \code{type = "cg"} or class factor if \code{type = "db"} and for \code{fit.dbn}. Non-empty column names. 
+#   }
+#   \item{constraints}{an optional list of constraints on the edges for specifying required and forbidden edges for \code{fit.dbn}. See details.
+#   }
+#   \item{learn}{a boolean value. If TRUE (default), the network structure will be learnt. If FALSE, only conditional probabilities will be learnt (a graph must be provided in this case.)
+#   }
+#   \item{graph}{graph structure of class "graphNEL" or a data frame with two columns of (labeled "from" and "to"), containing a set of edges to be included in the graph to be provided if learn == FALSE. See details.
+#   }
+#   \item{type}{specify the type of network for \code{fit.gnbp}. \code{"cg"} for \code{Conditional Gaussian} (default) and \code{"db"} for \code{Discrete Bayesian}.
+#   }
+#   
+#   \item{method}{a character string. The score-based or constraint-based algorithms available in the package \pkg{bnlearn}. Valid options are \code{"hc"}, \code{"tabu"}, \code{"gs"}, \code{"iamb"}, \code{"fast.iamb"}, \code{"inter.iamb"}, \code{"mmhc"}. See details below. 
+#   }
+#   
+#   \item{whitelist}{a data frame with two columns of (labeled "from" and "to"), containing a set of edges to be included in the graph.
+#   }
+#   \item{blacklist}{a data frame with two columns (labeled "from" and "to"), containing a set of edges NOT to be included in the graph.
+#   }
+#   
+#   \item{alpha}{a single numeric value specifying the significance level (for use with RHugin). Default is 0.001.
+#   }
+#   \item{tol}{a positive numeric value (optional) specifying the tolerance for EM algorithm to learn conditional probability tables (for use with RHugin). Default value is 1e-04. See \code{learn.cpt} for details.
+#              
+#   }
+#   \item{maxit}{a positive integer value (optional) specifying the maximum number of iterations of EM algorithm to learn conditional probability tables (for use with RHugin). See \code{learn.cpt} for details.
+#   }
+# \value{
+#     \code{fit.gnbp} returns an object of class "gpfit" containing the following components.
+#     
+#     \item{gp}{a pointer to a compiled RHugin domain that is the inferred network structure and the conditional probability tables for each node in the network.}
+#     \item{gp_nodes}{a data frame containing information about nodes for internal use with other functions.}
+#     \item{gp_flag}{a character string specifying the type of network : "cg" for (\code{Conditional Gaussian} or "db"/"dbn" for \code{Discrete Bayesian})}
+#     
+#     \code{fit.dbn} returns an object of class "dbnfit" containing the following components. 
+#     \item{dbn}{an object of class bn. See \code{\linkS3class{bn-class}} for details. This object contains the inferred network structure and the conditional probability tables for each node in the network.}
+#     \item{dbn_nodes}{a data frame containing information about nodes for internal use with other functions.}
+#     \item{dbn_flag}{a character string specifying the type of network \code{"dbn"} for \code{Discrete Bayesian})}
+#   }
+
+###################################################################################
+## Learn Bayesian Network Structure
+## from eQTL data (all discrete variables)
+###################################################################################
+fit.dbn=function(geno,pheno,graph,learn="TRUE",method="hc",whitelist,blacklist)
+  
+{
+  
+  ## Geno Class Check ##
+  for(i in 1:dim(geno)[2])
+    if (class(geno[,i])!="factor")
+    {warning("column vectors of 'geno' are not of class factor. converting to factor...")
+     geno[,i]<-as.factor(geno[,i])
+    }
+  
+  ## Pheno Class Check ##
+  class_pheno=lapply(pheno,class)
+    X<-which(class_pheno=="factor")
+    if(length(X)!=dim(pheno)[2])
+      stop("column vectors of 'pheno' should be of class factor.")
+  
+  ## Combine data
+  Data=cbind(pheno,geno)
+  
+  if (learn=="TRUE")
+  {
+  #### Create the blacklist
+  ## Impose prior knowledge on the ordering of the variables....
+    # Block 2: SNPs (geno)
+    # Block 1: Genes (pheno)
+  block<-matrix(1,nrow=dim(Data)[2],ncol=1)#assign variables a block
+  block[(dim(pheno)[2]+1):dim(Data)[2]]<-2
+  block=as.vector(block)
+  blM <- matrix(0, nrow = dim(Data)[2], ncol = dim(Data)[2])
+  rownames(blM) <- colnames(Data)
+  colnames(blM) <- colnames(Data)
+  
+  # fill in the disallowed edges
+  blM[block <= 2, block ==2] <- 1
+  
+ if(!missing(blacklist))
+ {
+   for (i in dim(blacklist)[1])
+   blM[blacklist[,"from"],blacklist[,"to"]]<-1
+ }
+  
+  blackL <- data.frame(igraph::get.edgelist(as(blM, "igraph")))
+  names(blackL) <- c("from", "to")
+  
+  if(!missing(whitelist))
+    whiteL=whitelist
+  else
+   whiteL=NULL
+  
+  
+  ## bnlearn:: hc (Hill-climbing)  
+  if(method=="hc") 
+    Data.bn <- bnlearn::hc(Data, whitelist=whiteL,blacklist = blackL)
+ 
+  ## bnlearn:: tabu (TABU) 
+  if(method=="tabu") 
+    Data.bn <- bnlearn::tabu(Data, whitelist=whiteL,blacklist = blackL)
+  
+  ## bnlearn:: hc (Grow-Shrink)
+  if(method=="gs") 
+    Data.bn <- bnlearn::gs(Data, whitelist=whiteL,blacklist = blackL)
+  
+  ## bnlearn:: iamb (Incremental-Association)
+  if(method=="iamb") 
+    Data.bn <- bnlearn::iamb(Data, whitelist=whiteL,blacklist = blackL)
+  
+  ## bnlearn:: fast.iamb (Fast Incremental Association)
+  if(method=="fast.iamb") 
+    Data.bn <- bnlearn::fast.iamb(Data, whitelist=whiteL,blacklist = blackL)
+  
+  ## bnlearn:: inter.iamb (Interleaved Incremental Association)
+  if(method=="inter.iamb") 
+    Data.bn <- bnlearn::inter.iamb(Data, whitelist=whiteL,blacklist = blackL)
+
+  ## bnlearn:: MMHC (Max-Min Hill Climbing)
+  if(method=="mmhc") 
+    Data.bn <- bnlearn::mmhc(Data, whitelist=whiteL,blacklist = blackL)
+
+  # Fit the graphical independence network
+  network<-bnlearn::bn.fit(Data.bn,Data)
+ 
+ 
+ }else
+    
+  {
+    if (missing(graph))
+      stop("if learn == FALSE, a graph structure (graphNEL object or data.frame of edges) must be provided.")
+  
+    if(class(graph)=="graphNEL")
+    {
+      bngraph<-bnlearn::as.bn(graph)
+      # Fit the graphical independence network
+      network<-bnlearn::bn.fit(bngraph,Data)  
+    }
+    
+    if(class(graph)=="data.frame") 
+    {
+      ## get nodes
+      nodes<-c(colnames(geno),colnames(pheno))
+      ## create edge list
+      edgeL<-vector("list",length(nodes))
+      ## name the list
+      names(edgeL) <- nodes
+      ## add edges
+      for(i in 1:length(edgeL))
+        edgeL[[i]]<-list(edges=graph[which(graph[,1]==names(edgeL)[i]),2])
+      
+      ## create a graphNEL object
+      edge.graph<-graphNEL(names(edgeL),edgeL,edgemode="directed")
+      
+      bngraph<-bnlearn::as.bn(edge.graph)
+      # Fit the graphical independence network
+      network<-bnlearn::bn.fit(bngraph,Data)  
+    }
+
+  }
+  
+ 
+ ## Determine type of nodes (discrete or continuous)
+ class_nodes=matrix(nrow=dim(Data)[2],ncol=3)
+ 
+ for (i in 1:dim(Data)[2])
+   class_nodes[i,]=c(colnames(Data)[i],class(Data[,i]),length(levels(Data[,i])))
+ 
+ class_nodes=cbind(class_nodes,t(cbind(t(rep("pheno",dim(pheno)[2])),t(rep("geno",dim(geno)[2])))))
+ colnames(class_nodes)=c("node","class","levels","type")
+
+ 
+ dbnfit<-list(dbn=network,dbn_nodes=class_nodes,dbn_flag="dbn")
+ class(dbnfit)<-"dbnfit"
+ 
+ return(dbnfit)
+
+    
+}
+
---
+++ pkg/R/plot-methods.R
@@ -0,0 +1,420 @@
+# \name{plot-methods}
+# \alias{plot.gnbp}
+# \alias{plot.dbn}
+# \alias{plot.gpfit}
+# \alias{plot.dbfit}
+# \title{
+#   Plot a Genotype-Phenotype Network
+# }
+#   \usage{
+#   \method{plot}{gnbp}(x, y="JSI",ncol = 1, col.palette,col.length = 100, 
+#   fontsize=10, fontcolor="black",...)
+#   \method{plot}{dbn}(x, y="state",col.palette,col.length = 100, 
+#   fontsize=10, fontcolor="black",...)  
+#   \method{plot}{gpfit}(x, fontsize=10, fontcolor="black",...)
+#   \method{plot}{dbnfit}(x, fontsize=10, fontcolor="black",...)            
+#   
+#   }
+#   \arguments{
+#   \item{x}{
+#   An object of class "gnbp" (\code{plot.gnbp}), "dbn" (\code{plot.dbn}), "gpfit" (\code{plot.gpfit}) or "dbnfit" (\code{plot.dbnfit})
+#   }
+#   \item{y}{
+#   A character string. Valid options are \code{"JSI"} (default) or \code{"belief"} for Conditional Gaussian network and \code{"FC"} or \code{"state"} for Discrete Bayesian networks. \code{plot.dbn} plots only discrete bayesian networks. Note that \code{y} will be ignored for \code{plot.dbnfit} and \code{gpfit}
+#   }
+#   \item{ncol}{a positive integer specifying the column number of JSI / belief / FC to plot. By default, the first column will be used.}
+#   \item{col.palette}{A list of character strings. For \code{"JSI"},\code{"belief"} and \code{"FC"} a list of 6 elements specifying colors for colormap.All 6 elements should be character strings specifying the colour for 
+#   \code{pos_high}= high end of gradient of positive values (default = "red" (JSI, belief), "darkmagenta",(FC))
+#   \code{pos_low}=low end of gradient of positive values (default = "wheat1" (JSI, belief), "palegoldenrod",(FC))
+#   \code{neg_high}=high end of gradient of positive values (default = "cyan" (JSI, belief), "palegoldenrod",(FC))
+#   \code{neg_low}=low end of gradient of positive values (default = "blue" (JSI, belief), "gold2",(FC))
+#   \code{dsep_col}= \emph{d}-separated nodes (default = "white")
+#   \code{qtl_col}= discrete nodes (QTLs) (default = "grey")
+#   \code{node_abs_col}= nodes for which evidence has been absorbed (default = "palegreen2")
+#   
+#   For \code{"state"}, a list of 4 elements specifying colors for colormap should be specified. All 4 elements should be character strings specifying the colour for 
+#   \code{col_nodes}- a vector of colors for phenotype states should be specified. The length of the vector should be equal to the maximum number of phenotype states possible.
+#   \code{dsep_col}= \emph{d}-separated nodes (default = "white")
+#   \code{qtl_col}= discrete nodes (QTLs) (default = "grey")
+#   \code{node_abs_col}= nodes for which evidence has been absorbed (default = "palegreen2")
+#   
+#   }
+#   \item{col.length}{
+#   a positive integer (default = 100) specifying the resolution of the colormap (number of colors) in case of \code{"belief"}, \code{"JSI"} and \code{"FC"}. For \code{"state"}, this argument will be ignored.
+#   }
+#   \item{fontsize}{a single numeric value. fontsize for node labels}
+#   \item{fontcolor}{fontcolor for node labels}
+#   \item{...}{further arguments to the function \code{\link{plot}}. These will be ignored}
+#   }
+#############################################################################
+
+## plot.gpfit plots output from fit.gnbp
+plot.gpfit=function(x,fontsize=10, fontcolor="black",...)
+{
+  ## get node attributes
+  class_nodes=x$gp_nodes
+  
+  requireNamespace("RHugin") || stop("Package not loaded: RHugin");
+  
+  ## convert to graphNEL object for use with Rgraphviz
+  BNgraph<-RHugin::as.graph.RHuginDomain(x$gp)
+  
+  ## set node attributes
+  z<-graph::nodes(BNgraph)
+  names(z)<-graph::nodes(BNgraph)
+  
+  nAttrs <- list()
+  eAttrs <- list()
+  attrs<-list()
+  nAttrs$label<-z
+  
+  attrs$node$fontsize<-fontsize
+  attrs$node$fontcolor<-fontcolor
+  attrs$node$fixedsize<-F
+  attrs$node$height<-1.2
+  
+  nAttrs$shape<-rep("ellipse",length(z))
+  names(nAttrs$shape)<-graph::nodes(BNgraph)
+  nAttrs$shape[class_nodes[which(class_nodes[,"type"]=="geno"),1]]<-"box"
+  
+  plot(BNgraph, nodeAttrs=nAttrs,attrs=attrs) 
+  
+}
+
+
+## plot.dbnfit plots output from fit.gnbp
+plot.dbnfit=function(x,fontsize=10, fontcolor="black",...)
+{
+  ## get node attributes
+  class_nodes=x$dbn_nodes
+  
+  ## convert to graphNEL object for use with Rgraphviz
+  BNgraph<-as.graphNEL(x$dbn)
+  
+  ## set node attributes
+  z<-graph::nodes(BNgraph)
+  names(z)<-graph::nodes(BNgraph)
+  
+  nAttrs <- list()
+  eAttrs <- list()
+  attrs<-list()
+  nAttrs$label<-z
+  
+  attrs$node$fontsize<-fontsize
+  attrs$node$fontcolor<-fontcolor
+  attrs$node$fixedsize<-F
+  attrs$node$height<-1.2
+  
+  nAttrs$shape<-rep("ellipse",length(z))
+  names(nAttrs$shape)<-graph::nodes(BNgraph)
+  nAttrs$shape[class_nodes[which(class_nodes[,"type"]=="geno"),1]]<-"box"
+  
+  plot(BNgraph, nodeAttrs=nAttrs,attrs=attrs) 
+  
+}
+
+## plot.gnbp plots cgbn and discrete bayesian networks, output from absorb.gnbp
+plot.gnbp=function(x, y="JSI",ncol = 1, col.palette,col.length = 100, fontsize=10, fontcolor="black",...)
+
+{
+  ## get node attributes
+  class_nodes=x$gp_nodes
+  
+  ## get network type
+  type=x$gp_flag
+  
+ 
+  if(type=="cg")
+  {
+    requireNamespace("RHugin") || stop("Package not loaded: RHugin");
+  
+  ## extract data
+  Data<-RHugin::get.cases(x$gp)
+
+  ## get d-connected nodes
+  dnodes<-RHugin::get.dconnected.nodes(x$gp,x$node)
+  dnodes<-class_nodes[match(setdiff(class_nodes[match(dnodes,class_nodes),1],x$node),class_nodes),]
+  dnodes<-matrix(dnodes,ncol=4)
+  colnames(dnodes)<-colnames(class_nodes)
+  
+  ## convert to graphNEL object for use with Rgraphviz
+  BNgraph<-RHugin::as.graph.RHuginDomain(x$gp)
+  }
+  
+  if(type=="db")
+  {
+    ## convert to graphNEL object for use with Rgraphviz
+     BNgraph<-as.graphNEL(as.bn.fit(x$gp))
+     
+     ## get d-connected nodes
+     dnodes=c()
+     for (i in 1:dim(class_nodes)[1])
+     {
+       if(!dsep(as.bn.fit(x$gp),node,class_nodes[i,1]))
+         dnodes<-rbind(dnodes,class_nodes[i,])
+     }
+     dnodes<-dnodes[-which(dnodes[,1]==node),]
+  } 
+  
+  
+  ## set node attributes
+  z<-graph::nodes(BNgraph)
+  names(z)<-graph::nodes(BNgraph)
+  
+  nAttrs <- list()
+  eAttrs <- list()
+  attrs<-list()
+  nAttrs$label<-z
+  
+  attrs$node$fontsize<-fontsize
+  attrs$node$fontcolor<-fontcolor
+  attrs$node$fixedsize<-F
+  attrs$node$height<-1.2
+
+  
+  nAttrs$shape<-rep("ellipse",length(z))
+  names(nAttrs$shape)<-graph::nodes(BNgraph)
+  nAttrs$shape[class_nodes[which(class_nodes[,"type"]=="geno"),1]]<-"box"
+  
+  if (type == "cg")
+  {
+  
+      if(missing(col.palette))
+      col.palette<-list(pos_high="red", pos_low= "wheat1", 
+                        neg_high="cyan", neg_low = "blue",
+                        dsep_col="white",qtl_col="grey",node_abs_col="palegreen2") 
+      
+      
+      if (y=="JSI")
+      {score=x$JSI[,ncol]} else
+        {if (y=="belief")
+          {score=x$belief$pheno$mean[,ncol]} else
+            {warning("invalid option for the argument 'y': plotting JSI ....")
+             score=x$JSI[,ncol]}}
+      
+      X=length(dnodes[which(dnodes[,4]=="geno"),1])
+      nAttrs$fillcolor<-rep(col.palette$dsep_col,length(z))
+      names(nAttrs$fillcolor)<-graph::nodes(BNgraph)
+      
+        
+        score_no<-seq(floor(range(score)[1]),ceiling(range(score)[2]),length.out=col.length)
+        score_range<-cbind(score_no[-length(score_no)],score_no[-1])
+        score_int<-findInterval(score,sort(score_no))
+      
+        if (!is.null(x$node) || !is.null(score))
+        {
+          if (length(which(score_no<0))!=0)
+           {colormap1<-cscale(1:(length(which(score_no<0))-1),palette=seq_gradient_pal(low=col.palette$neg_low,high=col.palette$neg_high))}   
+          else
+            {colormap1<-NULL}
+          if (length(which(score_no>0))!=0)
+            colormap2<-cscale(1:(length(which(score_no>0))-1),palette=seq_gradient_pal(low=col.palette$pos_low,high=col.palette$pos_high))   
+          else
+            colormap2<-NULL
+          
+          colormap_network<-c(colormap1,colormap2)[score_int]
+          
+          for (i in 1:length(x$node))
+          {
+            colormap_network<-c(colormap_network,col.palette$node_abs_col)
+          }
+          
+          colormap_network<-c(colormap_network, rep(col.palette$qtl_col,X))
+          
+          names(colormap_network)=c(c(names(score),x$node),dnodes[which(dnodes[,4]=="geno"),1])
+          nAttrs$fillcolor<-colormap_network
+          
+#           non_dnodes<-setdiff(class_nodes[,1],names(colormap_network))
+          colormap<-c(c(colormap1,col.palette$dsep_col),colormap2)
+          
+          
+          layout(matrix(c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,2,2,0), 4, 6, byrow = F))
+          
+#           layout(matrix(c(1,2), 1, 2, byrow = F),c(4,1),c(1,1))
+
+          plot(BNgraph, nodeAttrs=nAttrs,attrs=attrs) 
+    
+          scale = (length(colormap)-1)/(round(max(score_no))-round(min(score_no)))
+          ticks=seq(round(min(score_no)),0, len=3)
+          ticks<-ticks[-3]
+          ticks=c(ticks,seq(0,round(max(score_no)),len=3))
+          
+          plot(c(0,10), c(round(min(score_no)),round(max(score_no))), type='n', bty='n', xaxt='n', xlab='', yaxt='n', ylab='')
+          axis(2, pos=3,ticks, las=1,font=1)
+          for (i in 1:(length(colormap)-1)) 
+            {
+              y = (i-1)/scale + round(min(score_no))
+              rect(3.05,y,4.05,y+(1/scale), col=colormap[i], border=NA)
+            }
+        
+       }
+  }
+   
+invisible(x)
+
+}
+
+#################################################################################################
+## plot.dbn : plots discrete bayesian networks. output from absorb.dbn
+#################################################################################################
+plot.dbn=function(x, y="state",col.palette,col.length = 100, fontsize=10, fontcolor="black",...)
+  
+{
+  ## get node attributes
+  class_nodes=x$gp_nodes
+  
+  ## get network type
+  type=x$gp_flag
+  
+  ## convert to graphNEL object for use with Rgraphviz
+  BNgraph<-as.graphNEL(as.bn.fit(x$gp))
+    
+  ## get d-connected nodes
+  dnodes=c()
+  for (i in 1:dim(class_nodes)[1])
+  {
+    if(!dsep(as.bn.fit(x$gp),node,class_nodes[i,1]))
+        dnodes<-rbind(dnodes,class_nodes[i,])
+  }
+    dnodes<-dnodes[-which(dnodes[,1]==node),] 
+  
+  
+  ## set node attributes
+  z<-graph::nodes(BNgraph)
+  names(z)<-graph::nodes(BNgraph)
+  
+  nAttrs <- list()
+  eAttrs <- list()
+  attrs<-list()
+  nAttrs$label<-z
+  
+  attrs$node$fontsize<-fontsize
+  attrs$node$fontcolor<-fontcolor
+  attrs$node$fixedsize<-F
+  attrs$node$height<-1.2
+  
+  nAttrs$shape<-rep("ellipse",length(z))
+  names(nAttrs$shape)<-graph::nodes(BNgraph)
+  nAttrs$shape[class_nodes[which(class_nodes[,"type"]=="geno"),1]]<-"box"
+
+    
+    if(y=="FC")
+    {
+      
+      if(missing(col.palette))
+        colpalette=list(pos_high="darkmagenta",pos_low="palegoldenrod",
+                        neg_high="palegoldenrod",neg_low="gold2",
+                        dsep_col="white",qtl_col="grey",node_abs_col="palegreen2")
+      
+      score=x$FC$FC[,ncol]
+      
+      X=length(dnodes[which(dnodes[,4]=="geno"),1])
+      nAttrs$fillcolor<-rep(col.palette$dsep_col,length(z))
+      names(nAttrs$fillcolor)<-graph::nodes(BNgraph)
+      
+      score_no<-seq(floor(range(score)[1]),ceiling(range(score)[2]),length.out=col.length)
+      score_range<-cbind(score_no[-length(score_no)],score_no[-1])
+      score_int<-findInterval(score,sort(score_no))
+      
+      if (!is.null(x$node) || !is.null(score))
+      {
+        if (length(which(score_no<1))!=0)
+        {colormap1<-cscale(1:(length(which(score_no<1))-1),palette=seq_gradient_pal(low=col.palette$neg_low,high=col.palette$neg_high))}   
+        else
+        {colormap1<-NULL}
+        if (length(which(score_no>1))!=0)
+          colormap2<-cscale(1:(length(which(score_no>1))-1),palette=seq_gradient_pal(low=col.palette$pos_low,high=col.palette$pos_high))   
+        else
+          colormap2<-NULL
+        
+        colormap_network<-c(colormap1,colormap2)[score_int]
+        
+        for (i in 1:length(x$node))
+        {
+          colormap_network<-c(colormap_network,col.palette$node_abs_col)
+        }
+        
+        colormap_network<-c(colormap_network, rep(col.palette$qtl_col,X))
+        
+        names(colormap_network)=c(c(names(score),x$node),dnodes[which(dnodes[,4]=="geno"),1])
+        nAttrs$fillcolor<-colormap_network
+        
+        #           non_dnodes<-setdiff(class_nodes[,1],names(colormap_network))
+        colormap<-c(colormap1,colormap2)
+        
+        
+        layout(matrix(c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,2,2,0), 4, 6, byrow = F))
+        
+        #           layout(matrix(c(1,2), 1, 2, byrow = F),c(4,1),c(1,1))
+        
+        plot(BNgraph, nodeAttrs=nAttrs,attrs=attrs) 
+        
+        scale = (length(colormap)-1)/(round(max(score_no))-round(min(score_no)))
+        ticks=seq(round(min(score_no)),0, len=3)
+        ticks<-ticks[-3]
+        ticks=c(ticks,seq(0,round(max(score_no)),len=3))
+        
+        plot(c(0,10), c(round(min(score_no)),round(max(score_no))), type='n', bty='n', xaxt='n', xlab='', yaxt='n', ylab='')
+        axis(2, pos=3,ticks, las=1,font=1)
+        for (i in 1:(length(colormap)-1)) 
+        {
+          y = (i-1)/scale + round(min(score_no))
+          rect(3.05,y,4.05,y+(1/scale), col=colormap[i], border=NA)
+        }
+        
+      }
+      
+      
+    }
+    
+    if(y=="state")
+    {
+      if(missing(col.palette))
+        col.palette<-list(col_nodes=c("Red","Blue","Orange","Yellow","Magenta","Brown"),
+                          dsep_col="white",qtl_col="grey",node_abs_col="palegreen2") 
+     
+      ## generate colors for node states
+      colormap<-dscale(factor(x$FC$pheno_state[,ncol]),manual_pal(col.palette$col_nodes))
+      
+      ## add color for absorbed node
+      for (i in 1:length(x$node))
+      {
+        colormap_network<-c(colormap,col.palette$node_abs_col)
+      }
+      
+  
+      ## add color for d-connected qtl nodes
+      X=length(dnodes[which(dnodes[,4]=="geno"),1])
+      colormap_network<-c(colormap_network, rep(col.palette$qtl_col,X))
+      
+      ## set node names to colormap_network
+      names(colormap_network)=c(c(names(colormap),x$node),dnodes[which(dnodes[,4]=="geno"),1])
+      
+      ## set default color of all nodes
+      nAttrs$fillcolor<-rep(col.palette$dsep_col,length(z))
+      names(nAttrs$fillcolor)<-graph::nodes(BNgraph)
+      
+      ## set color for all nodes
+      nAttrs$fillcolor<-colormap_network
+      
+      layout(matrix(c(1,2), 1, 2, byrow = F),c(4,1),c(1,1))
+      
+      plot(BNgraph, nodeAttrs=nAttrs,attrs=attrs)
+      
+      node_leg=c(paste("state",c(1:max(dnodes[which(dnodes[,4]=="pheno"),"levels"]))))
+      names(node_leg)=col.palette$col_nodes[1:length(node_leg)]
+      
+      leg=c(node_leg,"d-separated","d-connected","absorbed node")
+      leg.col=c(names(node_leg),col.palette$dsep_col,col.palette$qtl_col,col.palette$node_abs_col)  
+      
+      pch=c(rep(21,length(node_leg)),22,22,21)
+      
+      plot(c(1:length(leg)),type='n', bty='n', xaxt='n', xlab='', yaxt='n', ylab='',bg=leg.col,col="black",pch=pch)
+      legend("center",legend=leg,col=par("col"),pt.bg=leg.col,pch=pch,bty='n',pt.cex=1.5,cex=0.8)
+    } 
+   
+    
+  
+  invisible(x)
+  
+}
